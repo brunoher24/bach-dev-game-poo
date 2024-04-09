3686dd172cbab095ce630f1e71606b233a347712
Bruno Héritier <brunoheritier@macbook-pro-de-bruno.home>
2024-04-09 14:17:44 +0200

    init board works but only draw html

--- js/index.js
+++ js/index.js
@@ -1,3 +1,42 @@
 // consigne 1
 // créer une classe Board qui prend en paramètre (dans la fonction constructor)
 // - num_of_columns, num_of_squares
+class Board {
+  constructor(columns, squares) {
+    this.columns = columns;
+    this.squares = squares;
+    this.draw();
+  }
+
+  draw() {
+    const mainCtnr = document.querySelector("#game-board");
+    let innerHTML = "";
+    for (let x = 0; x < this.columns; x++) {
+      innerHTML += this.drawColumn();
+    }
+    mainCtnr.innerHTML = innerHTML;
+  }
+
+  drawColumn() {
+    let innerHTML = "";
+    innerHTML += `<div class="column">`;
+    for (let y = 0; y < this.squares; y++) {
+      innerHTML += `<div class="square"></div>`;
+    }
+    innerHTML += `</div>`;
+    return innerHTML;
+  }
+}
+
+const board = new Board(10, 10);
+
+
+
+
+
+
+
+
+
+
+
